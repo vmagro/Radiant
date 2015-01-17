3ee5e668ed491c83bc7c01ca43896984771353c7
David Carr <d@dcarr.io>
2015-01-17 17:02:39 -0500

    working on knockout

--- coffee/index.coffee
+++ coffee/index.coffee
@@ -1,13 +1,18 @@
 do sensor_status = ->
+  @frontLeft = ko.observable 4
+  @frontRight = ko.observable 4
+  @leftEar = ko.observable 4
+  @rightEar = ko.observable 4
+
   $(".index-li").addClass "active"
 
   socket = io.connect(window.location.hostname)
   socket.on "news", (value) ->
     return  unless value.args and value.address is "/muse/elements/horseshoe"
-    frontLeft = value.args[0]
-    frontRight = value.args[1]
-    leftEar = value.args[2]
-    rightEar = value.args[3]
+    @frontLeft = value.args[0]
+    @frontRight = value.args[1]
+    @leftEar = value.args[2]
+    @rightEar = value.args[3]
 
     console.log 'front left: ' + frontLeft
     console.log 'front right: ' + frontRight
--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,8 @@
 var sensor_status;
 
 (sensor_status = function() {
-  var socket;
+  var ko, socket;
   $(".index-li").addClass("active");
   socket = io.connect(window.location.hostname);
-  return socket.on("news", function(value) {
-    var frontLeft, frontRight, leftEar, rightEar;
-    if (!(value.args && value.address === "/muse/elements/horseshoe")) {
-      return;
-    }
-    frontLeft = value.args[0];
-    frontRight = value.args[1];
-    leftEar = value.args[2];
-    rightEar = value.args[3];
-    console.log('front left: ' + frontLeft);
-    console.log('front right: ' + frontRight);
-    console.log('left ear: ' + leftEar);
-    return console.log('right ear: ' + rightEar);
-  });
+  return socket.on("news", value);
 })();
