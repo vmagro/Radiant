5019ae82436c315bc6aad82376efbc8a5d7b7df9
David Carr <d@dcarr.io>
2015-01-17 20:20:59 -0500

    color bind

--- coffee/light-controls.coffee
+++ coffee/light-controls.coffee
@@ -1,21 +1,4 @@
-light1 = ko.observable(0x000099)
-light2 = ko.observable(0x000099)
-light3 = ko.observable(0x000099)
-light4 = ko.observable(0x000099)
-
-light1Hex = ko.computed =>
-  '#' + ('00000000' + light1().toString(16)).slice(-6)
-
-light2Hex = ko.computed =>
-  '#' + ('00000000' + light2().toString(16)).slice(-6)
-
-light3Hex = ko.computed =>
-  '#' + ('00000000' + light3().toString(16)).slice(-6)
-
-light4Hex = ko.computed =>
-  '#' + ('00000000' + light4().toString(16)).slice(-6)
-
-lightObservables = [light1, light2, light3, light4]
+lightObservables = [app.light1, app.light2, app.light3, app.light4]
 
 onChangeFor = (pickerNum) ->
   return (container, color) ->
--- public/js/app-all.js
+++ public/js/app-all.js
@@ -8,6 +8,30 @@ App = (function() {
     this.rightEar = ko.observable(4);
     this.mellowGraphData = ko.observableArray();
     this.concentrationGraphData = ko.observableArray();
+    this.light1 = ko.observable(0x000099);
+    this.light2 = ko.observable(0x000099);
+    this.light3 = ko.observable(0x000099);
+    this.light4 = ko.observable(0x000099);
+    this.light1Hex = ko.computed((function(_this) {
+      return function() {
+        return '#' + ('00000000' + _this.light1().toString(16)).slice(-6);
+      };
+    })(this));
+    this.light2Hex = ko.computed((function(_this) {
+      return function() {
+        return '#' + ('00000000' + _this.light2().toString(16)).slice(-6);
+      };
+    })(this));
+    this.light3Hex = ko.computed((function(_this) {
+      return function() {
+        return '#' + ('00000000' + _this.light3().toString(16)).slice(-6);
+      };
+    })(this));
+    this.light4Hex = ko.computed((function(_this) {
+      return function() {
+        return '#' + ('00000000' + _this.light4().toString(16)).slice(-6);
+      };
+    })(this));
   }
 
   return App;
